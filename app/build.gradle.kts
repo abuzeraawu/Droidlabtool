d86d57382f6b9beef9e56d52fc7582fb944409c2
azers <gizfixer@gmail.com>
2023-11-28 14:52:11 +0530

    ar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.auazer.droidlabtool"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/auazer/droidlabtool/MainActivity.java
+++ app/src/main/java/com/auazer/droidlabtool/MainActivity.java
@@ -1,16 +1,33 @@
 package com.auazer.droidlabtool;
 
+
+
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private BillingClient billingClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,43 +39,107 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // Set a title (optional)
-        Objects.requireNonNull(getSupportActionBar()).setTitle("Your App Title");
+        Objects.requireNonNull(getSupportActionBar()).setTitle("droid lab");
+
+        PurchasesUpdatedListener purchasesUpdatedListener = new PurchasesUpdatedListener() {
+            @Override
+            public void onPurchasesUpdated(BillingResult billingResult, List<com.android.billingclient.api.Purchase> purchases) {
+                // To be implemented in a later section.
+            }
+        };
+
+        billingClient = BillingClient.newBuilder(this)
+                .setListener(purchasesUpdatedListener)
+                .enablePendingPurchases()
+                .build();
+
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(BillingResult billingResult) {
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                    // The BillingClient is ready. You can query purchases here.
+                }
+            }
+
+            @Override
+            public void onBillingServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+            }
+        });
 
         // Connect the CardViews
         CardView cardView1 = findViewById(R.id.cardView1);
-        CardView cardView2 = findViewById(R.id.cardView2);
-        CardView cardView3 = findViewById(R.id.cardView3);
-        CardView cardView4 = findViewById(R.id.cardView4);
-        CardView cardView5 = findViewById(R.id.cardView5);
 
-        // Add a click listener to CardView1
-        cardView1.setOnClickListener(view -> {
-            // Start VersionuninstallActivity
-            startActivity(new Intent(MainActivity.this, VersionuninstallActivity.class));
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "CardView1 clicked");
+
+                // Check if billing client is connected before initiating purchase
+                if (billingClient.isReady()) {
+                    Log.d(TAG, "BillingClient is ready. Initiating purchase...");
+
+                    // Start VersionuninstallActivity
+                    startActivity(new Intent(MainActivity.this, VersionuninstallActivity.class));
+
+                    // Initiate purchase for a specific product (replace "your_product_sku_here" with the actual SKU)
+                    initiatePurchase();
+                } else {
+                    Log.e(TAG, "BillingClient not ready. Unable to initiate purchase.");
+                    // Handle the error or retry connection
+                }
+            }
         });
 
-        // Add a click listener to CardView2
-        cardView2.setOnClickListener(view -> {
-            // Navigate to PhoneunlockActivity
-            startActivity(new Intent(MainActivity.this, PhoneunlockActivity.class));
-        });
 
-        // Add a click listener to CardView3
-        cardView3.setOnClickListener(view -> {
-            // Navigate to PrivatesafeunlockActivity
-            startActivity(new Intent(MainActivity.this, PrivatesafeunlockActivity.class));
-        });
 
-        // Add a click listener to CardView4
-        cardView4.setOnClickListener(view -> {
-            // Navigate to ContactNumberrecoveryActivity
-            startActivity(new Intent(MainActivity.this, ContactNumberrecoveryActivity.class));
-        });
 
-        // Add a click listener to CardView5
-        cardView5.setOnClickListener(view -> {
-            // Navigate to CallLogrecActivity
-            startActivity(new Intent(MainActivity.this, CallLogrecActivity.class));
-        });
+
+
+
+
+
+
+
     }
-}
+
+    // Example method for starting the purchase flow
+    private void initiatePurchase() {
+        // Use BillingClient to query for the SkuDetails
+        SkuDetailsParams params = SkuDetailsParams.newBuilder()
+                .setSkusList(Arrays.asList("15rupees"))
+                .setType(BillingClient.SkuType.INAPP)  // For in-app products
+                .build();
+
+        billingClient.querySkuDetailsAsync(params, (billingResult, skuDetailsList) -> {
+            if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && skuDetailsList != null) {
+                // SkuDetails retrieval successful, launch the billing flow
+                for (SkuDetails skuDetails : skuDetailsList) {
+                    // Display product information if needed
+                    String title = skuDetails.getTitle();
+                    String price = skuDetails.getPrice();
+
+
+                    // Now, launch the billing flow
+                    BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
+                            .setSkuDetails(skuDetails)
+                            .build();
+
+                    BillingResult responseCode = billingClient.launchBillingFlow(this, billingFlowParams);
+
+                    // Handle the response code if needed
+                    if (responseCode.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                        // Purchase initiated successfully
+                    } else {
+                        // Handle the error
+                        Log.e(TAG, "Error initiating purchase: " + responseCode.getDebugMessage());
+                    }
+                }
+            } else {
+                // Handle the error
+                Log.e(TAG, "Error retrieving SkuDetails: " + billingResult.getDebugMessage());
+            }
+        });
+    }}
+
